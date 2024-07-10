4c274ddc735ac669465e0a3ddfc54ce97156158e
zeinmubaraq <zainmubaraq@gmail.com>
2024-07-10 13:32:14 +0700

    fix responsive login1 page

--- src/app/forms/login/login-1/page.tsx
+++ src/app/forms/login/login-1/page.tsx
@@ -4,8 +4,8 @@ import Link from 'next/link';
 export default function Login1() {
   return (
     <div className="flex h-screen items-center justify-center bg-gray-200">
-      <div className="flex h-4/5 w-3/5 justify-center">
-        <div className="relative w-6/12 rounded-l-md bg-yellow-300 shadow-lg">
+      <div className="flex h-4/5 w-10/12 justify-center lg:w-3/5">
+        <div className="relative hidden w-6/12 rounded-l-md shadow-lg lg:block">
           <Image
             src="/bg-login1.png"
             alt="login 1"
@@ -14,7 +14,7 @@ export default function Login1() {
             className="rounded-l-md"
           />
         </div>
-        <div className="w-5/12 rounded-r-md bg-white px-10 py-14 shadow-lg">
+        <div className="w-full rounded-r-md bg-white px-10 py-14 shadow-lg lg:w-5/12">
           <div className="flex justify-center">
             <Image src="/eastblue.svg" alt="eastblue logo" width={130} height={10} />
           </div>
@@ -26,16 +26,20 @@ export default function Login1() {
               </label>
               <input
                 type="text"
+                name="username"
+                id="username"
                 className="block w-full border-b py-1.5 text-sm outline-none focus:border-blue-500"
               />
             </div>
 
             <div className="mt-5">
-              <label htmlFor="username" className="block text-xs font-medium text-gray-500">
+              <label htmlFor="password" className="block text-xs font-medium text-gray-500">
                 Password
               </label>
               <input
                 type="password"
+                id="password"
+                name="password"
                 className="block w-full border-b py-1.5 text-sm outline-none focus:border-blue-500"
               />
             </div>
